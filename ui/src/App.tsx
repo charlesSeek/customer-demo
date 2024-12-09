f42d7a43b0d5d97c4c9c180746c3f1e1ac4f91c5
shuchengc <cuishucheng@gmail.com>
2024-12-10 08:10:26 +1100

    feat: update styles

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Layout } from "antd";
 import Home from './pages/Home'
+import './App.css'; 
 
 const { Content } = Layout;
 
 const App = () => {
   return (
     <Layout>
-      <Content style={{ padding: "50px" }}>
+      <Content className="responsive-content">
         <Home />
       </Content>
     </Layout>
