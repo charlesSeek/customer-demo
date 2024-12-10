4f2c68a1f04c81d7d3a3b4376fca4ae2be42a5a5
shuchengc <cuishucheng@gmail.com>
2024-12-11 08:09:14 +1100

    chore: update filter test

--- ui/src/components/Filter/Filter.test.tsx
+++ ui/src/components/Filter/Filter.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Filter from './Filter';
 import userEvent from '@testing-library/user-event';
 import { AppStateContext, initialState } from '../../hooks/useCustomers';
